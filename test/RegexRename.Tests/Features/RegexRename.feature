ef4ac127460b32c09c47a3d8fc424588adad8391
Jim Dale <26042891+jim-dale@users.noreply.github.com>
2023-01-09 08:56:04 +0000

    added some more SpecFlow tests

--- test/RegexRename.Tests/Features/RegexRename.feature
+++ test/RegexRename.Tests/Features/RegexRename.feature
@@ -1,13 +1,13 @@
-﻿Feature: Calculator
+﻿Feature: RegexRename
 
-Scenario: Get a list of files matching the pattern *.json in a folder
+Scenario: Get a list of files matching the pattern *.json in a given folder
 	When the 'test-data' folder is searched for files matching '*.json'
 	Then the files list should be:
 		| file                   |
 		| test-data\\file 1.json |
 		| test-data\\file 2.json |
 
-Scenario: Get a list of files matching the pattern *.json in a folder and all sub-folders
+Scenario: Get a list of files matching the pattern *.json in a given folder and all sub-folders
 	When the 'test-data' folder is searched for files matching '**/*.json'
 	Then the files list should be:
 		| file                              |
@@ -15,3 +15,14 @@ Scenario: Get a list of files matching the pattern *.json in a folder and all su
 		| test-data\\file 2.json            |
 		| test-data\\subfolder\\file 1.json |
 		| test-data\\subfolder\\file 2.json |
+
+Scenario: File processor
+	Given I have a file name processor with the following parameters:
+		| Input Pattern            | Output Pattern                  | Variables               |
+		| file (?<FileNumber>\\d+) | {FileNumber:D4} - new file name | FileNumber,System.Int32 |
+	When I transform the following file names:
+		| Input       |
+		| file 1.json |
+	Then I expect the following output:
+		| Output               |
+		| 0001 - new file name |
---
+++ test/RegexRename.Tests/Steps/FileNameProcessorSteps.cs
@@ -0,0 +1,82 @@
+﻿namespace RegexRename.Tests.Steps;
+
+using System.Linq;
+using FluentAssertions.Execution;
+using RegexRename.Support;
+
+[Binding]
+public sealed class FileNameProcessorSteps
+{
+    private FileNameProcessor? sut;
+    private readonly List<string> output = new();
+
+    [Given(@"I have a file name processor with the following parameters:")]
+    public void GivenIHaveAFileNameProcessorWithTheFollowingParameters(Table table)
+    {
+        ArgumentNullException.ThrowIfNull(table, nameof(table));
+
+        using (var scope = new AssertionScope())
+        {
+            table.Rows.Count.Should().BeGreaterThan(0);
+            table.Rows[0].Count.Should().Be(3);
+        }
+
+        var inputPattern = table.Rows[0][0];
+        var outputPattern = table.Rows[0][1];
+        var variables = table.Rows[0][2];
+
+        inputPattern.Should().NotBeNullOrWhiteSpace();
+        outputPattern.Should().NotBeNullOrWhiteSpace();
+        variables.Should().NotBeNullOrWhiteSpace();
+
+        var variableTypes = new Dictionary<string, string>();
+        var variableDefinitions = variables.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        foreach (var variableDefinition in variableDefinitions)
+        {
+            var nameAndType = variableDefinition.Split(',', 2, StringSplitOptions.TrimEntries);
+
+            variableTypes.Add(nameAndType[0], nameAndType[1]);
+        }
+
+        this.sut = new FileNameProcessor(inputPattern, outputPattern, variableTypes);
+    }
+
+    [When(@"I transform the following file names:")]
+    public void WhenITransformTheFollowingFileNames(Table table)
+    {
+        ArgumentNullException.ThrowIfNull(table, nameof(table));
+
+        this.sut.Should().NotBeNull();
+        using (var scope = new AssertionScope())
+        {
+            table.Rows.Count.Should().BeGreaterThan(0);
+            table.Rows[0].Count.Should().Be(1);
+        }
+
+        foreach (var row in table.Rows)
+        {
+            var output = this.sut!.TransformFileName(row[0]);
+            output.Should().NotBeNull();
+            this.output.Add(output!);
+        }
+    }
+
+    [Then(@"I expect the following output:")]
+    public void ThenIExpectTheFollowingOutput(Table table)
+    {
+        ArgumentNullException.ThrowIfNull(table, nameof(table));
+
+        using (var scope = new AssertionScope())
+        {
+            table.Rows.Count.Should().BeGreaterThan(0);
+            table.Rows[0].Count.Should().Be(1);
+        }
+
+        var expected = table.Rows.Select(r => r[0]).ToList();
+
+        expected.Sort();
+        this.output.Sort();
+
+        expected.Should().BeEquivalentTo(this.output);
+    }
+}
